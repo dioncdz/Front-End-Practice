2af5b3968ca2cf85f3bd7091da249bb749be6839
dioncdz <dioncadizjr@gmail.com>
2020-08-22 03:22:22 +0800

    improved and cleaned the code

--- 01- Basic JS/script.js
+++ 01- Basic JS/script.js
@@ -1,16 +1,20 @@
 let buttons = document.querySelectorAll('.bg-btn');
 
-buttons.forEach(button => button.addEventListener('click', ()=>{
-   if (button.className.includes('dark')) {
+function changeBg(btn) {
+
+   if (btn.target.className.includes('dark')) {
       document.querySelector('#background-change').style.backgroundColor = '#568259';
    } 
-   else if (button.className.includes('medium')) {
+   else if (btn.target.className.includes('medium')) {
       document.querySelector('#background-change').style.backgroundColor = '#96E6B3';
    } 
    else {
       document.querySelector('#background-change').style.backgroundColor = '#CCFCCB';
-   }
-}));
+   };
+
+}
+
+buttons.forEach(button => button.addEventListener('click', changeBg));
 
 
 
