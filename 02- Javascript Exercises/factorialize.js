9bdbe44ca86cbe7fb7aff0f9c8b43dbdd628d4ce
dioncdz <dioncadizjr@gmail.com>
2020-09-04 01:58:55 +0800

    Factorialize / LongestWord

---
+++ 02- Javascript Exercises/factorialize.js
@@ -0,0 +1,20 @@
+/*********************************
+ * // FACTORIALIZE A NUMBER
+  *********************************/
+
+ function factorialize(num) {
+   if(num < 1) {
+     return 1;
+   } else {
+   return num * factorialize(num - 1) ;
+   }
+ }
+
+ /****************************
+
+console.log(factorialize(5)) // should return 120
+console.log(factorialize(10)) // should return 3628800
+console.log(factorialize(20)) // should return 2432902008176640000
+console.log(factorialize(0)) // should return 1
+
+ ***************************/
---
+++ 02- Javascript Exercises/findLongestWord.js
@@ -0,0 +1,35 @@
+/*********************************
+ * // FIND LONGEST WORD
+  *********************************/
+
+function findLongestWordLength(str) {
+   let arr = str.split(' ').sort((a, b) => b.length - a.length);
+   
+   return arr[0].length;
+ }
+
+ /****************************
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog")) // should return 6.
+
+console.log(findLongestWordLength("May the force be with you")) // should return 5.
+
+console.log(findLongestWordLength("Google do a barrel roll")) // should return 6.
+
+console.log(findLongestWordLength("What is the average airspeed velocity of an unladen swallow")) // should return 8.
+
+console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology")) // should return 19.
+
+ ***************************/
+
+
+/**************
+ * // OTHER SOLUTIONS
+  
+ * //USING MAPS
+
+function findLongestWordLength(str) {
+   return Math.max(...str.split(" ").map(word => word.length));
+}
+
+*/
